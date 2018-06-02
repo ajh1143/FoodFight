abc638d013e93b9504693c538a9d0cc7bbcb5b4e
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-06-01 17:30:12 -0700

    Create BrawlerGenerator.py

---
+++ BrawlerGenerator.py
@@ -0,0 +1,42 @@
+import random
+class BrawlerGenerator(object):
+
+    def hungerGenerator(self):
+        hungerRand = random.randint(1,100)
+        return hungerRand
+
+
+    def toppingGenerator(self):
+        burgerToppings = ['Cheddar', 'Jalapeno', 'Bacon', 'Pineapple']
+        return random.choice(burgerToppings)
+
+
+    def eatActionGenerator(self):
+        actions = ['Gorge', 'Devour', 'Ingest']
+        return random.choice(actions)
+
+
+    def angerActionGenerator(self):
+        angers = ['Throw', 'Slam', 'Rip', 'Smash']
+        return random.choice(angers)
+
+
+    def favFoodGenerator(self):
+        favfood = ['Cheeseburger', 'Basic Burger', 'Taco']
+        return random.choice(favfood)
+
+
+    def nameGenerator(self):
+        names = ['Terry', 'Eric', 'Jennifer', 'Ashley', 'Gerald']
+        return random.choice(names)
+
+    def generateBrawler(self):
+        hunger = self.hungerGenerator()
+        topping = self.toppingGenerator()
+        eatAction = self.eatActionGenerator()
+        angerAction = self.angerActionGenerator()
+        favFood = self.favFoodGenerator()
+        name = self.nameGenerator()
+        atts = [hunger, topping, eatAction, angerAction, favFood, name]
+        Brawler = Snob(*atts)
+        return Brawler
