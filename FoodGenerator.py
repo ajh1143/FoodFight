a2b24856b73ea6efcfb842ff2519d11a91072f63
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-06-01 19:13:29 -0700

    Delete FoodGenerator.py

--- FoodGenerator.py
+++
@@ -1,8 +0,0 @@
-class FoodGenerator(object):
-
-    def FoodTypeGenerator(self):
-        run = BrawlerGenerator()
-        food_type = run.favFoodGenerator()
-        topping = run.toppingGenerator()
-        food = [food_type, topping]
-        return food
