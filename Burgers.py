d29954b382d6cf2364c809775d42c6bf54afdc2b
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-06-01 18:13:46 -0700

    Update Burgers.py

--- Burgers.py
+++ Burgers.py
@@ -1,26 +1,24 @@
 class BasicBurger():
+
+
     def __init__(self, patties=1, bun=True):
         self.patties = patties
         self.bun = bun
         self.food_type = "Basic Burger"
-
-    def __str__(self):
-        return "A basic burger has: {} Meat patties, and a bun.".format(self.patties)
+        self.topping = None
 
 
 class Cheeseburger(BasicBurger):
+
+
     def __init__(self, cheesetype ="Cheddar", cheesecount=1, patties = 1):
         self.cheesetype = cheesetype
         self.cheesecount = cheesecount
         self.patties =patties
+        self.food_type = "Cheeseburger"
+        self.topping = None
 
 
-    def __str__(self):
-        return "This cheeseburger has: {} Meat patties, {} cheese slices of {} type.".format(self.patties,
-                                                                                             self.cheesecount,
-                                                                                             self.cheesetype)
-
-    
     def setMeatPatties(self, newPatties):
         self.patties = newPatties
 
@@ -43,7 +41,6 @@ class Cheeseburger(BasicBurger):
         self.cheesecount = slices
         return self.cheesecount
 
-    
+
     def getCheeseSlices(self):
-      #  print("This cheeseburger has {} cheese slices".format(self.patties))
         return self.patties
